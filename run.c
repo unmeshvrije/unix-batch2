eaffe47ea1cc0881b4b094368240ad49c4d975dd
Unmesh Unix Lover <unmeshjoshiofficial@gmail.com>
2026-01-21 20:04:09 +0100

    Update comment in run.c

--- run.c
+++ run.c
@@ -23,8 +23,19 @@ int main(int argc, char *argv[]) {
    *
    * ./mycp = we want to run this program
    *
-   * exec (  , mycp, hello.c, hello_again.c)
    *
+   * exec (mycp , mycp, hello.c, hello_again.c)
+   *
+   *  1st mycp = path to the program that is to be executable
+   *
+   *  2nd mycp = First argument (arg0) of that program
+   *
+   *  By coincidence, they are both same, because we are running "run"
+   *  from the directory where "mycp" binary exists.
+   *
+   *
+   *  If the executable has a different path somewhere, then
+   *  execl ("/some/other/path/mycp", "mycp", ...)
    * */
 
   /* assume 2 args: src and target file */
